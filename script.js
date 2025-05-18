8e9b1532aa18a2f43550e190b1f0e98cd3caf58d
gadgeski <xtwh.ce5bil@gmail.com>
2025-05-18 15:27:10 +0900

    Updated index.html and more

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ thumbnails.forEach((thumb) => {
     mainImg.animate({ opacity: [0, 1] }, 500);
   });
 });
+
+// ローディング用
+const loadingElement = document.getElementById("loading");
+
+// 進捗状況用数値
+let progress = 0;
+
+// 10ミリ秒毎に数値を加算、テキストを更新
+const intervalId = setInterval(() => {
+  progress++;
+  loadingElement.textContent = progress + "%";
+
+  // 100%到達時、ローディングを終了、フェードアウト
+  if (progress >= 100) {
+    clearInterval(intervalId);
+    loadingElement.classList.add("fade-out");
+  }
+}, 10);
